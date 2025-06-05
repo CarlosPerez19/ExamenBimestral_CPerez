3a1b913d271dab372599945dfe230c6376e0c524
null <null>
2025-06-05 16:45:45 -0500

    Eliminacion credenciales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyBOK2CSebyRePx3HxRci1WPdGVbmnjf0R8",
-    authDomain: "examenbimestral-58936.firebaseapp.com",
-    projectId: "examenbimestral-58936",
-    storageBucket: "examenbimestral-58936.firebasestorage.app",
-    messagingSenderId: "1073136117735",
-    appId: "1:1073136117735:web:f414ce85cbb6e9c8e7bf42",
-    measurementId: "G-9DYGX2M54L"
-  },
+  production: true
 };
 
 /*
